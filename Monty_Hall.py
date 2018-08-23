26ee59e765551970da90c3aae74de29cc7f82825
andyum <19matroskin89>
2018-08-23 23:41:59 +0300

    second

--- Monty_Hall.py
+++ Monty_Hall.py
@@ -5,7 +5,7 @@ stubborn = 0  # счетчик выигрышей упертого игрока
 unstable = 0  # счетчик выигрышей переменчивого игрока
 for i in range(1000):
     shuffle(doors)  # перемешиваем массив doors
-    f = randint(0, 2)
+    f = randint(0, 2)  # генерируем выбор двери
     stubborn += doors[f]
     if doors[f] == 0:
         unstable += 1
