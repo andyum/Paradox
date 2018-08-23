84713ec0df1a149f901ae28bb203efcfd3f05512
andyum <19matroskin89>
2018-08-23 23:51:21 +0300

    more comments

--- 2_envelopes.py
+++ 2_envelopes.py
@@ -1,15 +1,16 @@
 from random import randint, shuffle
 
-envelopes = [200, 100]
-stubborn = 0
-unstable = 0
+envelopes = [200, 100]  # 2 конверта с наличкой в 100 и 200 валюты
+stubborn = 0  # счетчик выигрышей упертого игрока
+unstable = 0  # счетчик выигрышей переменчивого игрока
 for i in range(1000):
-    shuffle(envelopes)
-    f = randint(0, 1)
-    stubborn += envelopes[f]
+    shuffle(envelopes)  # перемешиваем конверты
+    f = randint(0, 1)  # генерируем выбор конверта
+    stubborn += envelopes[f]  # + упертому
     if f == 0:
-        unstable += envelopes[1]
+        unstable += envelopes[1]  # +1 переменчивому
     else:
-        unstable += envelopes[0]
+        unstable += envelopes[0]  # +1 переменчивому
+
 print('Stubborn:', stubborn)
 print('Unstable:', unstable)
