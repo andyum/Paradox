9e6fceb462b96f58bc20ccfc57614319d618fc57
andyum <19matroskin89>
2018-08-23 23:30:11 +0300

    first

---
+++ Monty_Hall.py
@@ -0,0 +1,14 @@
+from random import randint, shuffle
+
+doors = [1, 0, 0]  # 3 двери. 1 - приз, 0 - проигрыш
+stubborn = 0  # счетчик выигрышей упертого игрока
+unstable = 0  # счетчик выигрышей переменчивого игрока
+for i in range(1000):
+    shuffle(doors)  # перемешиваем массив doors
+    f = randint(0, 2)
+    stubborn += doors[f]
+    if doors[f] == 0:
+        unstable += 1
+
+print('Stubborn:', stubborn)
+print('Unstable:', unstable)
